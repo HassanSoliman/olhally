dd9042e8a1d02c9b7ddb8450a70798f8e42fdbe3
Hassan Soliman <hassansoliman573@gmail.com>
2017-04-17 15:26:46 +0000

    adding sass gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
-    
-   def index
-       
-       
-       
-   end
-    
+  def index
+  end
+
+  def inbox
+  end
+
+  def contact
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
   root to: 'pages#index'
+
+  get 'pages/inbox'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
 end
